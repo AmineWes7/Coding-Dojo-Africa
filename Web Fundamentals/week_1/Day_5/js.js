b4bf32c1bfe7070fa862e5047592ae8df5e488b6
AmineWes7 <156659557+AmineWes7@users.noreply.github.com>
2024-02-06 13:17:43 +0100

    Add files via upload

---
+++ Web Fundamentals/week_1/Day_5/js.js
@@ -0,0 +1,29 @@
+/*for (var i=1 ;i<20;i++){
+    if (i%2==1)
+    console.log(i)
+    
+    
+}*/
+/*for (var i=100 ;i>=0;i--){
+    if (i%3==0)
+    console.log(i)
+
+}*/
+/*var s=4
+for (var i=0 ;i<6;i++){
+    console.log(s)
+    s=s-1.5
+    
+}*/
+/*var sum=0
+for (var i=1 ;i<101;i++){
+    
+    sum=sum+i
+}
+console.log(sum)*/
+/*var sum=1
+for (var i=1 ;i<13;i++){
+    
+    sum=sum*i
+}
+console.log(sum)*/
