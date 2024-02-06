63b8c1ff4be55a3daecb67dddf31adbc125580df
AmineWes7 <156659557+AmineWes7@users.noreply.github.com>
2024-02-06 22:40:56 +0100

    Add files via upload

--- Web Fundamentals/week_2/Day_1/script.js
+++ Web Fundamentals/week_2/Day_1/script.js
@@ -1,3 +1,11 @@
-
-
-console.log("page loaded...");
+function logout(element){
+    console.log(element.innerHTML)
+    element.innerHTML = "Logout"
+}
+function button(param){
+    console.log(param)
+    param.remove()
+}
+function ninja(){
+    alert('ninja was liked')
+}
