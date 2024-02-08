5c649fb1d1c553e666252012b42a21df6d1e98aa
AmineWes7 <156659557+AmineWes7@users.noreply.github.com>
2024-02-08 14:21:09 +0100

    Add files via upload

--- Web Fundamentals/week_2/Day_2/script.js
+++ Web Fundamentals/week_2/Day_2/script.js
@@ -1,11 +1,18 @@
-function logout(element){
-    console.log(element.innerHTML)
-    element.innerHTML = "Logout"
-}
-function button(param){
-    console.log(param)
-    param.remove()
-}
-function ninja(){
-    alert('ninja was liked')
-}
+const myButton = document.querySelector('#myButton');
+const myText = document.querySelector('#myText');
+
+myButton.addEventListener('click', () => {
+    myText.textContent = '10 Like(s)';
+});
+const myButton2 = document.querySelector('#myButton2');
+const myText2 = document.querySelector('#myText2');
+
+myButton2.addEventListener('click', () => {
+    myText2.textContent = '13 Like(s)';
+});
+const myButton3 = document.querySelector('#myButton3');
+const myText3 = document.querySelector('#myText3');
+
+myButton3.addEventListener('click', () => {
+    myText3.textContent = '10 Like(s)';
+});
